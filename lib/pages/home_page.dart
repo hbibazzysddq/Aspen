184eeb53c4c2190f4d0cd99f873e1c7eb7686288
hbibazzysddq <habibsiddiq169@gmail.com>
2024-11-21 14:20:01 +0700

    Home Page 4/4

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,102 +1,369 @@
 import 'package:belajar_flutter/components/menu_aspen.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void onTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    const background = Colors.white;
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                margin: const EdgeInsets.only(top: 48, left: 20),
-                child: const Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Explore',
-                      style: TextStyle(
-                        fontFamily: 'Montserrat',
-                        fontSize: 18,
-                        fontWeight: FontWeight.w300,
+      backgroundColor: background,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(top: 48, left: 20),
+                  child: const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Explore',
+                        style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontSize: 18,
+                          fontWeight: FontWeight.w300,
+                        ),
                       ),
-                    ),
-                    Text(
-                      'Aspen',
-                      style: TextStyle(
-                        fontFamily: 'Montserrat',
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold,
+                      Text(
+                        'Aspen',
+                        style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(
-                  right: 20,
-                ),
-                child: const Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.location_on_sharp,
-                      color: Colors.blue,
-                      size: 20,
-                    ),
-                    SizedBox(
-                      width: 5,
-                    ),
-                    Text(
-                      'Aspen, USA',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 16,
-                      ),
-                    ),
-                    SizedBox(
-                      width: 5,
-                    ),
-                    Icon(
-                      Icons.keyboard_arrow_down,
-                      color: Colors.blue,
-                      size: 22,
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-          Container(
-            margin: const EdgeInsets.only(top: 24, right: 20, left: 20),
-            child: TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
+                    ],
                   ),
-                  prefixIcon: const Icon(
-                    Icons.search,
-                    color: Colors.grey,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(
+                    right: 20,
                   ),
-                  hintText: 'Find things to do',
-                  hintStyle: const TextStyle(
-                    color: Colors.grey,
-                  )),
-              onChanged: (value) {
-                // Handle text input change here
-              },
+                  child: const Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.location_on_sharp,
+                        color: Colors.blue,
+                        size: 20,
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        'Aspen, USA',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 16,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Icon(
+                        Icons.keyboard_arrow_down,
+                        color: Colors.blue,
+                        size: 22,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(top: 32, left: 20),
-            child: const MenuAspen(),
-          ),
-        ],
+            Container(
+              margin: const EdgeInsets.only(top: 24, right: 20, left: 20),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    prefixIcon: const Icon(
+                      Icons.search,
+                      color: Colors.grey,
+                    ),
+                    hintText: 'Find things to do',
+                    hintStyle: const TextStyle(
+                      color: Colors.grey,
+                    )),
+                onChanged: (value) {
+                  // Handle text input change here
+                },
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 32, left: 18),
+              child: const MenuAspen(),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 32, left: 20, right: 20),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Popular',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                      fontFamily: 'Montserrat',
+                    ),
+                  ),
+                  Text(
+                    'See All',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(
+                top: 24,
+                left: 20,
+                right: 20,
+              ),
+              height: 320,
+              child: ListView(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Stack(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image.asset(
+                          'image/populer1.png',
+                          fit: BoxFit.cover,
+                          width: 200,
+                          height: 320,
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.bottomLeft,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                color: Colors.black.withOpacity(0.7),
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              padding: const EdgeInsets.only(
+                                  top: 6, right: 6, bottom: 6, left: 6),
+                              margin: const EdgeInsets.only(
+                                left: 16,
+                                bottom: 12,
+                              ),
+                              child: const Text(
+                                'Mountains',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600,
+                                  fontFamily: 'Montserrat',
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(width: 16),
+                  Stack(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image.asset(
+                          'image/populer2.png',
+                          fit: BoxFit.cover,
+                          width: 200,
+                          height: 320,
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.bottomLeft,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                color: Colors.black.withOpacity(0.7),
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              padding: const EdgeInsets.only(
+                                  top: 6, right: 6, bottom: 6, left: 6),
+                              margin: const EdgeInsets.only(
+                                left: 16,
+                                bottom: 12,
+                              ),
+                              child: const Text(
+                                'Coeurdes Alpes',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600,
+                                  fontFamily: 'Montserrat',
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Container(
+                margin: const EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                  top: 20,
+                ),
+                child: const Text(
+                  'Recomended',
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                      fontFamily: 'Montserrat'),
+                ),
+              ),
+            ),
+            Container(
+              height: 160,
+              margin: const EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+                bottom: 49,
+              ),
+              decoration: const BoxDecoration(
+                  // color: Colors.blue,
+                  ),
+              child: ListView(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16),
+                          border: Border.all(color: Colors.grey.shade200),
+                          color: Colors.white,
+                        ),
+                        child: Column(
+                          children: [
+                            Image.asset(
+                              'image/recomended.png',
+                              width: 166,
+                              height: 102,
+                            ),
+                            const SizedBox(height: 10),
+                            const Text(
+                              'Explore Aspen',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontFamily: 'Montserrat',
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 16,
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16),
+                          border: Border.all(color: Colors.grey.shade200),
+                          color: Colors.white,
+                        ),
+                        child: Column(
+                          children: [
+                            Image.asset(
+                              'image/recomended1.png',
+                              width: 166,
+                              height: 102,
+                            ),
+                            const SizedBox(height: 10),
+                            const Text(
+                              'Explore Aspen',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontFamily: 'Montserrat',
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: ClipRRect(
+        borderRadius: const BorderRadius.vertical(
+          top: Radius.circular(32),
+          bottom: Radius.zero,
+        ),
+        child: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.airplane_ticket_rounded), label: 'Ticket'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.favorite), label: 'Favorites'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.blue,
+          unselectedItemColor: Colors.grey,
+          onTap: onTapped,
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.white,
+        ),
       ),
     );
   }
